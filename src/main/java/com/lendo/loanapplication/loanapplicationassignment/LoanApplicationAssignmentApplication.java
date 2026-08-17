a6b028227aa6c3fcdf1831695f5e31ba2ab1dbb2
lamanagabekova <laman.aghabayova_ges@omio.com>
2026-08-17 18:48:45 +0200

    Setting new project

---
+++ src/main/java/com/lendo/loanapplication/loanapplicationassignment/LoanApplicationAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.lendo.loanapplication.loanapplicationassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplicationAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplicationAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lendo/loanapplication/loanapplicationassignment/LoanApplicationAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lendo.loanapplication.loanapplicationassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
